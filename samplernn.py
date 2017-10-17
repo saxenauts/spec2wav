4e3bb53e4e161a983ad3d395cc5d3d404de953f4
Saxenauts <saxenauts@gmail.com>
2017-10-18 00:29:57 +0530

    TierRNN Class Basic Structure

--- samplernn.py
+++ samplernn.py
@@ -51,7 +51,7 @@ class TopFrameInput(torch.nn.Module):
         #TODO: Weight Initializations for QRNN
 
         self.conv1d = torch.nn.Conv1d(
-                        q_levels,
+                        q_levels,           #TODO: hidden, input dims?
                         q_levels,
                         kernel_size = 2,
                         stride = 1)
@@ -60,14 +60,13 @@ class TopFrameInput(torch.nn.Module):
     def forward(self, audio, spectrogram):
 
         #TODO: cuda
-
         spectro_qrnn, hidden_1 = self.qrnn1(spectrogram, hidden0_1)
-        spectro_qrnn_b, hidden_1_b = self.qrnn1_b(utils.reverse(spectro_qrnn_1_b), \
+        spectro_qrnn_b, hidden_1_b = self.qrnn1_b(utils.reverse(spectro_qrnn_1_b, 2), \
                                             hidden0_1_b)
         stack_1 = torch.stack([spectro_qrnn_1, spectro_qrnn_1_b])
 
         spectro_qrnn, hidden_2 = self.qrnn2(stack_1, hidden0_2)
-        spectro_qrnn_b, hidden_2_b = self.qrnn2_b(utils.reverse(stack_1),\
+        spectro_qrnn_b, hidden_2_b = self.qrnn2_b(utils.reverse(stack_1, 2),\
                                             hidden0_2_b)
 
         stack_2 = torch.stack([spectro_qrnn, spectro_qrnn_b])
@@ -75,14 +74,50 @@ class TopFrameInput(torch.nn.Module):
         #TODO: Interleaving
         #TODO: spectrogram convolution
 
-        upsampled_spectro = stack_2.repeat(1, ratio) #TODO: for batches,
-                                                     # proper dimensions
+        upsampled_spectro = stack_2.repeat(1, 1, ratio)
 
+        #TODO: TODO TODO Verify this convolution method
         audio_input = self.conv1d(audio)
         return audio_input + upsampled_spectro
 
 
-class FrameLevelRNN(torch.nn.Module):
+class TierRNN(torch.nn.Module):
+    '''
+    Generates RNNs, can be used to generate RNN for different tiers.
+    Need to provide the frame_size, input_size, sequence_length, number of rnnns.
 
-    def __init__(self, frame_size):
-        pass
+    Input: prev rnn cell's hidden state, input samples, upsampled conditioning
+    '''
+    def __init__(self, frame_size, n_rnn, input_dim, seq_len):
+        super.__init__()
+
+        self.frame_size = frame_size
+        self.n_rnn = n_rnn
+
+        self.clock_input = torch.nn.Conv1d(
+                    in_channels = frame_size,
+                    out_channels = input_dim,
+                    kernel_size = 1
+        )
+
+        #TODO: Initialize weight for clock_input
+
+        self.rnn = torch.nn.GRU(
+            input_size = input_dim,
+            hidden = input_dim,
+            num_layers = n_rnn,
+            batch_first = True       #Input, Output provided as (Batch x Sequence x Feature)
+        )
+
+        #TODO : Initialize the RNNs
+
+        self.tier_output_upsample = utils.LearnedUpsampling1d(
+                in_channels = input_dim,
+                out_channels = input_dim,
+                kernel_size = frame_size
+        )
+        #TODO: Check this, and apply Initialization
+
+    def forward(self, prev_samples, upper_tier_conditioning, hidden):
+
+        self.tier_input =
--- utils.py
+++ utils.py
@@ -4,9 +4,48 @@ import numpy as np
 
 
 def reverse(tensor, dim):
+    #TODO: What is the input, and what is the return?
     np_arr = np.flip(tensor.numpy(),dim).copy()
     return torch.from_numpy(np_arr)
 
 
 def upsample(tensor, ratio):
-    return old.repeat(1, ratio)
+    return old.repeat(1, 1, ratio)
+
+class LearnedUpsampling1d(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size, bias=True):
+        super().__init__()
+
+        self.conv_t = nn.ConvTranspose1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=kernel_size,
+            bias=False
+        )
+
+        if bias:
+            self.bias = nn.Parameter(
+                torch.FloatTensor(out_channels, kernel_size)
+            )
+        else:
+            self.register_parameter('bias', None)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.conv_t.reset_parameters()
+        nn.init.constant(self.bias, 0)
+
+    def forward(self, input):
+        (batch_size, _, length) = input.size()
+        (kernel_size,) = self.conv_t.kernel_size
+        bias = self.bias.unsqueeze(0).unsqueeze(2).expand(
+            batch_size, self.conv_t.out_channels,
+            length, kernel_size
+        ).contiguous().view(
+            batch_size, self.conv_t.out_channels,
+            length * kernel_size
+        )
+        return self.conv_t(input) + bias
